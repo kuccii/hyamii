853d76cdc4a1faa2e2344621d27eed54fca4eb8c
kuccii <ypattos@gmail.com>
2026-05-24 14:14:52 +0200

    Remove storage cache from tracking

--- storage/framework/views/05659ae2017508578e2b80e0e5aaa0da.php
+++
@@ -1,8 +0,0 @@
-<style>
-    :root {
-        --color-base: <?php echo e($baseColor); ?>;
-        --color-secondary: 163, 59, 56;
-        --livewire-progress-bar-color: <?php echo e($baseColorHex); ?>;
-    }
-</style>
-<?php /**PATH C:\xamp\htdocs\Hyamii\resources\views/sections/theme_style.blade.php ENDPATH**/ ?>
